3d43129014134f6d08c22a1744a385d54890a84b
ksharma6 <ksharma6@tutanota.com>
2025-02-06 16:58:55 -0800

    added plot_images()

--- src/file_io.py
+++ src/file_io.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import cv2
+
 def txt_reader(path_to_txt):
     """Reads txt file and returns a list of lines with index removed
 
@@ -14,7 +17,6 @@ def txt_reader(path_to_txt):
     cleaned_content = []
     for s in temp:
         parts = s.split(' ', 1)
-
         if len(parts) > 1:
             new_string = parts[1]
         else:
@@ -22,3 +24,19 @@ def txt_reader(path_to_txt):
         cleaned_content.append(new_string)
 
     return cleaned_content
+
+def plot_images(file_paths, nrows, ncols):
+    """plot image data into matplotlib plot
+
+    Args:
+        file_paths (list): list of image url's to plot
+        nrows (int): number of rows for matplotlib plot
+        ncols (int): number of cols for matplotlib plot
+    """
+    for i, file in enumerate(file_paths):
+        img = cv2.imread(file, 1)
+        plt.subplot(nrows,ncols,i+1)
+        plt.title(img.shape, fontsize = 8)
+        plt.axis('off')
+        plt.imshow(img[:,:,::-1])
+        plt.tight_layout()
